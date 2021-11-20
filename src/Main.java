ca9cde00b0cac4e9622a16a767944de787cc98a2
Jakub Landowski <landowski222@gmail.com>
2021-11-20 10:46:32 +0100

    for loop commit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [] tab0 = {0,1,2,3,4,5,6};
-        for (int i = 0; i < tab0.length; i++) {
-            if(tab0[i]<3)
+        /*int [] tab0 = {0,1,2,3,4,5,6};
+        for (int j : tab0) {
+            if (j < 3)
                 System.out.println("True");
-            else if(tab0[i]<5)
+            else if (j < 5)
                 System.out.println("False");
+        }*/  // Table in for loop
 
+        /*String[] cars = {"Audi","BMW","VW","Łada"};
+        for (String car:cars){
+            System.out.println(car);
         }
+        for (String auto : cars) {
+            System.out.println("2nd loop "+auto);
+        }
+        int [] tab1 = {6, 5, 4, 3, 2, 1};
+        for (int number : tab1) {
+            if (number>3 && number<5)
+                System.out.println(number);
+        }*/     // for each loop
     }
 }
