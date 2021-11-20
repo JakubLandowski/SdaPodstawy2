fc380a9863428deb06025ab56e1697ddaa216519
Jakub Landowski <landowski222@gmail.com>
2021-11-20 11:14:23 +0100

    break continue commit

--- src/Main.java
+++ src/Main.java
@@ -54,5 +54,15 @@ public class Main {
         }
         while (j<5);*/     // do while
 
+        /*for (int i = 0; i < 10; i++) {
+            if (i == 5)
+                break;
+            System.out.println(i);
+        for (int i = 0; i < 10; i++) {
+            if (i == 5)
+                continue;
+            System.out.println(i);
+        }*/     // break continue
+
     }
 }
