720b642f27f9ebbeb735c5c545da3e3d45504c51
Jakub Landowski <landowski222@gmail.com>
2021-11-20 10:02:12 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] tab0 = {0,1,2,3,4,5,6};
+        for (int i = 0; i < tab0.length; i++) {
+            if(tab0[i]<3)
+                System.out.println("True");
+            else if(tab0[i]<5)
+                System.out.println("False");
+
+        }
+    }
+}
